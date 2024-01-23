0d4560c13968095289db7f4fd705a7084b2c0b0e
joselenon <jjoseguilhermeneto@gmail.com>
2024-01-23 16:08:35 -0300

    bora

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -15,8 +15,6 @@ import {
   tracingHandlerMiddleware,
 } from '../middlewares/sentryMiddlewares';
 import URLS, { API_BASE } from '../config/constants/URLS';
-import AxiosService from './AxiosService';
-import { ItemMarketData } from '../config/interfaces/ItemStatsComponentsProps';
 
 class AppService {
   private app: express.Application;
@@ -71,72 +69,9 @@ class AppService {
     this.setupEndpoints();
     this.setupWebSocket(); // Adicione a configuração do WebSocket
 
-    const activeIntervals: NodeJS.Timeout[] = [];
-
-    const fetchItemListings = async (itemName: string) => {
-      console.log('ENTROU AQUI, CUIDADO PRA N DAR MERDA');
-      console.log(itemName, '- Fetching!');
-
-      const response = await AxiosService<ItemMarketData>({
-        url: `https://pixels-server.pixels.xyz/v1/marketplace/item/${itemName}`,
-      });
-      const data = response.data as ItemMarketData;
-
-      return { [itemName]: { ...data } };
-    };
-
     this.server.listen(URLS.MAIN_URLS.SERVER_PORT, () =>
       console.log(`Server started, ${URLS.MAIN_URLS.SERVER_FULL_URL}`),
     );
-
-    this.wss.on('connection', (ws) => {
-      console.log('WebSocket connection established.');
-
-      ws.on('message', (message: string[]) => {
-        while (activeIntervals.length > 0) {
-          clearInterval(activeIntervals[0]);
-          activeIntervals.pop();
-        }
-        activeIntervals.forEach((interval) => clearInterval(interval));
-        if (message.length <= 0) return;
-
-        console.log('INTERVALS AGORA', activeIntervals);
-
-        const messageReceivedParsed = JSON.parse(message);
-        console.log('mensagem recebida', messageReceivedParsed);
-
-        const intervalId = setInterval(() => {
-          Promise.all(
-            messageReceivedParsed.map((itemName: string) => fetchItemListings(itemName)),
-          )
-            .then((result: { [itemName: string]: ItemMarketData }[]) => {
-              const objToSend: { [itemName: string]: ItemMarketData } = {};
-
-              result.forEach((itemMarketData, i) => {
-                console.log('OBJ SENT: ', messageReceivedParsed[i]);
-
-                objToSend[messageReceivedParsed[i]] = {
-                  listings: itemMarketData[messageReceivedParsed[i]].listings,
-                  ownerUsernames: itemMarketData[messageReceivedParsed[i]].ownerUsernames,
-                };
-              });
-
-              ws.send(JSON.stringify(objToSend));
-
-              console.log('Listagens enviadas com sucesso.');
-            })
-            .catch((error) => {
-              console.error('Erro ao buscar listagens:', error);
-            });
-        }, 6000);
-
-        activeIntervals.push(intervalId);
-      });
-
-      ws.on('close', () => {
-        console.log('WebSocket connection closed.');
-      });
-    });
   }
 }
 
