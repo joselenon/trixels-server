ffc993cc13ee31a107270cb30858935660bfebfd
joselenon <jjoseguilhermeneto@gmail.com>
2024-07-11 09:03:41 -0300

    pqp

--- src/config/constants/ENVIRONMENT.ts
+++ src/config/constants/ENVIRONMENT.ts
@@ -8,21 +8,30 @@ dotenv.config({ path: envPath });
 const ENVIRONMENT = {
   MODE: process.env.MODE as 'DEVELOPMENT' | 'PRODUCTION',
   HTTPS: process.env.HTTPS === 'true' ? true : false,
-  DOMAIN: process.env.CLIENT_DOMAIN || 'localhost',
-  SERVER_SUBDOMAIN_DOMAIN: process.env.SERVER_SUBDOMAIN_DOMAIN || 'localhost',
+
+  DOMAIN: process.env.CLIENT_DOMAIN,
+
+  SERVER_SUBDOMAIN_DOMAIN: process.env.SERVER_SUBDOMAIN_DOMAIN,
   SERVER_PORT: process.env.SERVER_PORT || '4000',
-  CLIENT_SUBDOMAIN_DOMAIN: process.env.CLIENT_SUBDOMAIN_DOMAIN || 'localhost',
+
+  CLIENT_SUBDOMAIN_DOMAIN: process.env.CLIENT_SUBDOMAIN_DOMAIN,
   CLIENT_PORT: process.env.CLIENT_PORT || '3000',
+
   REDIS_HOST: process.env.REDIS_HOST as string,
   REDIS_PORT: process.env.REDIS_PORT as string,
   REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
+
   SENTRY_DSN: process.env.SENTRY_DSN as string,
+
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID as string,
   FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY as string,
   FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL as string,
+
   SESSION_SECRET: process.env.SESSION_SECRET as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
+
   SKY_MAVIS_WEBHOOK_SIGNATURE: process.env.SKY_MAVIS_WEBHOOK_SIGNATURE as string,
+
   GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID as string,
   GOOGLE_OAUTH_CLIENT_SECRET_KEY: process.env.GOOGLE_OAUTH_CLIENT_SECRET_KEY as string,
 };
