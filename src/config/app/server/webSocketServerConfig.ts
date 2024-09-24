2a9362d7bd0821e2eafff5a4d0ae4e3c25a57d5f
joselenon <jjoseguilhermeneto@gmail.com>
2024-09-24 03:00:50 -0300

    ...

--- src/config/app/server/webSocketServerConfig.ts
+++ src/config/app/server/webSocketServerConfig.ts
@@ -2,6 +2,7 @@ import { Server } from 'http';
 import URLS from '../../constants/URLS';
 
 const webSocketServerConfig = (httpServer: Server) => {
+  console.log('httpServer', httpServer);
   return {
     server: httpServer,
     path: URLS.ENDPOINTS.GRAPHQL,
