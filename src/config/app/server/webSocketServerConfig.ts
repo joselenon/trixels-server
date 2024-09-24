c610bde9420307b44c91bafbaef9f664c4f6dc05
joselenon <jjoseguilhermeneto@gmail.com>
2024-09-24 03:13:55 -0300

    testing waters

--- src/config/app/server/webSocketServerConfig.ts
+++ src/config/app/server/webSocketServerConfig.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import URLS from '../../constants/URLS';
 
 const webSocketServerConfig = (httpServer: Server) => {
-  console.log('httpServer', httpServer);
   return {
     server: httpServer,
     path: URLS.ENDPOINTS.GRAPHQL,
--- src/routes/graphQLRouter.ts
+++ src/routes/graphQLRouter.ts
@@ -5,14 +5,12 @@ import bodyParser from 'body-parser';
 
 import context from '../config/graphql/context';
 import URLS, { API_BASE } from '../config/constants/URLS';
-import corsMiddleware from '../middlewares/corsMiddleware';
 
 const graphQLRouter = (apolloServer: ApolloServer) => {
   const graphQLRouters = Router();
 
   graphQLRouters.use(
     `${API_BASE}${URLS.ENDPOINTS.GRAPHQL}`,
-    corsMiddleware(),
     bodyParser.json(),
     expressMiddleware(apolloServer, { context }),
   );
